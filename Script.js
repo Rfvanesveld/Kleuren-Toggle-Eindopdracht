769d302ea646421ccba853b4b54d6b60790c7294
Rfvanesveld <75245865+Rfvanesveld@users.noreply.github.com>
2021-05-30 11:18:29 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,76 @@
+function openNav() {
+    document.getElementById("mySidebar").style.left = "10px";
+}
+function closeNav() {
+    document.getElementById("mySidebar").style.left = "-500px";
+}
+
+function silverTxt() {
+    document.getElementById("mysilverBG").style.display = "block";
+    document.getElementById("myblueBG").style.display = "none";
+    document.getElementById("myorangeBG").style.display = "none";
+    document.getElementById("myredBG").style.display = "none";
+    document.getElementById("mygreenBG").style.display = "none";
+}
+
+function blueTxt() {
+    document.getElementById("myblueBG").style.display = "block";
+    document.getElementById("mysilverBG").style.display = "none";
+    document.getElementById("myorangeBG").style.display = "none";
+    document.getElementById("myredBG").style.display = "none";
+    document.getElementById("mygreenBG").style.display = "none";
+}
+
+function orangeTxt() {
+    document.getElementById("myorangeBG").style.display = "block";
+    document.getElementById("mysilverBG").style.display = "none";
+    document.getElementById("myblueBG").style.display = "none";
+    document.getElementById("myredBG").style.display = "none";
+    document.getElementById("mygreenBG").style.display = "none";
+}
+
+function redTxt() {
+    document.getElementById("myredBG").style.display = "block";
+    document.getElementById("mysilverBG").style.display = "none";
+    document.getElementById("myblueBG").style.display = "none";
+    document.getElementById("myorangeBG").style.display = "none";
+    document.getElementById("mygreenBG").style.display = "none";
+}
+
+function greenTxt() {
+    document.getElementById("mygreenBG").style.display = "block";
+    document.getElementById("mysilverBG").style.display = "none";
+    document.getElementById("myblueBG").style.display = "none";
+    document.getElementById("myorangeBG").style.display = "none";
+    document.getElementById("myredBG").style.display = "none";
+}
+
+function Silver() {
+    document.bgColor = "Silver";
+    closeNav()
+    silverTxt()
+}
+
+function Blue() {
+    document.bgColor = "Cornflowerblue";
+    closeNav()
+    blueTxt()
+}
+
+function Orange() {
+    document.bgColor = "Orange";
+    closeNav()
+    orangeTxt()
+}
+
+function Red() {
+    document.bgColor = "Red";
+    closeNav()
+    redTxt()
+}
+
+function Green() {
+    document.bgColor = "Green";
+    closeNav()
+    greenTxt()
+}
